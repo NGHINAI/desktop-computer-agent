4795d9b7b199fb413589aa3e8c701d7be1499b99
NGHINAI <97311544+NGHINAI@users.noreply.github.com>
2025-04-15 20:37:39 +0530

    deleted previous and updated new files
    Bug fixes

---
+++ operater/main.py
@@ -0,0 +1,57 @@
+"""
+Self-Operating Computer
+"""
+import argparse
+import operator
+from utils.style import ANSI_BRIGHT_MAGENTA
+from operate import operator
+
+
+def main_entry():
+    parser = argparse.ArgumentParser(
+        description="Run the self-operating-computer with a specified model."
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="Specify the model to use",
+        required=False,
+        default="gpt-4-with-ocr",
+    )
+
+    # Add a voice flag
+    parser.add_argument(
+        "--voice",
+        help="Use voice input mode",
+        action="store_true",
+    )
+    
+    # Add a flag for verbose mode
+    parser.add_argument(
+        "--verbose",
+        help="Run operate in verbose mode",
+        action="store_true",
+    )
+    
+    # Allow for direct input of prompt
+    parser.add_argument(
+        "--prompt",
+        help="Directly input the objective prompt",
+        type=str,
+        required=False,
+    )
+
+    try:
+        args = parser.parse_args()
+        operator(
+            args.model,
+            terminal_prompt=args.prompt,
+            voice_mode=args.voice,
+            verbose_mode=args.verbose
+        )
+    except KeyboardInterrupt:
+        print(f"\n{ANSI_BRIGHT_MAGENTA}Exiting...")
+
+
+if __name__ == "__main__":
+    main_entry()
